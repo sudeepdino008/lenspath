6f7c17920f35ccef1146731102d5d9492c9939f3
sudeep <sudeepdino008@gmail.com>
2023-05-04 19:47:57 +0530

    fix to keep composed lenses unaffected of other lenses modifications

--- compose.go
+++ compose.go
@@ -3,8 +3,12 @@ package lenspath
 // this package provides ways to compose lenspath
 
 func (lp *Lenspath) Compose(lens []Lens) *Lenspath {
+	newlens := append(lp.lens, lens...)
+	copylens := make([]Lens, len(newlens))
+	copy(copylens, newlens)
+
 	return &Lenspath{
-		lens:      append(lp.lens, lens...),
+		lens:      copylens,
 		assumeNil: lp.assumeNil,
 	}
 }
--- compose_test.go
+++ compose_test.go
@@ -20,15 +20,39 @@ func TestCompose(t *testing.T) {
 		t.Errorf("composed: Expected %v, got %v", compose_expected, clp.lens)
 	}
 
-	clp = lp.Compose([]Lens{"f", "g"})
+	clp2 := lp.Compose([]Lens{"f", "g"})
 	compose_expected = []string{"a", "b", "c", "f", "g"}
+	if !reflect.DeepEqual(clp2.lens, compose_expected) {
+		t.Errorf("composed: Expected %v, got %v", compose_expected, clp2.lens)
+	}
+
+	// previous compose should hold
+	compose_expected = []string{"a", "b", "c", "d", "e"}
 	if !reflect.DeepEqual(clp.lens, compose_expected) {
 		t.Errorf("composed: Expected %v, got %v", compose_expected, clp.lens)
 	}
 
-	clp = clp.Compose([]Lens{"h", "i"})
+	clp = clp2.Compose([]Lens{"h", "i"})
 	compose_expected = []string{"a", "b", "c", "f", "g", "h", "i"}
 	if !reflect.DeepEqual(clp.lens, compose_expected) {
 		t.Errorf("composed: Expected %v, got %v", compose_expected, clp.lens)
+	}
+}
+
+func TestCompose2(t *testing.T) {
+	lp, _ := Create([]string{"a", "b", "c"})
+	clp := lp.Compose([]Lens{"d"})
+
+	clp2 := clp.Compose([]Lens{"f"})
+	clp3 := clp.Compose([]Lens{"g"})
+
+	expected := []string{"a", "b", "c", "d", "f"}
+	if !reflect.DeepEqual(clp2.lens, expected) {
+		t.Errorf("original: Expected %v, got %v", expected, clp2.lens)
+	}
+
+	expected = []string{"a", "b", "c", "d", "g"}
+	if !reflect.DeepEqual(clp3.lens, expected) {
+		t.Errorf("composed: Expected %v, got %v", expected, clp3.lens)
 	}
 }
